1d0a3040dc4345887159a30832abbacf61182ce2
Jefry Agustin Astacio Sanchez <jesanchez@DCTIC.GC>
2025-10-01 16:37:36 -0400

    feat: Configuración de Swagger y gestión de APP_NAME

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,40 @@
 import express ,{Request,Response}from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
 
 dotenv.config();
-
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 const port = process.env.PORT || 3001;
 
+// Opciones de Swagger
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: process.env.APP_NAME || 'API de Share Musician',
+      version: '1.0.0',
+      description: 'Documentación de la API para la aplicación Share Musician',
+    },
+    servers: [
+      {
+        url: `http://localhost:${port}`,
+        description: 'Servidor de desarrollo',
+      },
+    ],
+  },
+  apis: ['./src/**/*.ts'], // Rutas a los archivos de la API
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.get('/', (req:Request, res:Response) => {
-  res.send('¡Hola desde el backend de Share Musician!');
+  res.send(`¡Hola desde el backend de ${process.env.APP_NAME}!`);
 });
 
 app.listen(port, () => {
@@ -19,3 +42,4 @@ app.listen(port, () => {
 });
 
 // Mantener el proceso activo en entornos donde se cierra prematuramente
+process.stdin.resume();
